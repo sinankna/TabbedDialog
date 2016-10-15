ed94e308687257c55915acf3f0af0fdcac78cf5b
Sinan KNA <sinankna@gmail.com>
2016-10-16 02:17:52 +0300

    Added Title and Button options, like an Alert Dialog

--- tabbeddialog/src/main/java/com/sinankna/tabbeddialog/TabbedDialog.java
+++ tabbeddialog/src/main/java/com/sinankna/tabbeddialog/TabbedDialog.java
@@ -1,5 +1,9 @@
 package com.sinankna.tabbeddialog;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -9,6 +13,8 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.sinankna.tabbeddialog.utils.CustomViewPager;
 import com.sinankna.tabbeddialog.utils.TabbedDialogTemplate;
@@ -27,16 +33,40 @@ public class TabbedDialog extends DialogFragment {
     private TabLayout mTabLayout;
     private CustomViewPager mViewPager;
     private TabbedDialogsAdapter mAdapter;
+    private Button mOKButton;
 
-    @Nullable
-    @Override
+    private TextView mTVTitle;
+
+    private String mTitle,mOKButtonText;
+
+    private int mTitleColor,mOKButtonColor;
+
+    private View.OnClickListener mOKButtonClickListener;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.dialog_main,null);
 
+        mTVTitle = (TextView)view.findViewById(R.id.main_title);
+        mOKButton = (Button)view.findViewById(R.id.positive_button);
         mTabLayout = (TabLayout) view.findViewById(R.id.main_tablayout);
         mViewPager = (CustomViewPager) view.findViewById(R.id.main_viewpager);
         mAdapter = new TabbedDialogsAdapter(getChildFragmentManager(), mFragmentsList);
+
+
+        mTVTitle.setText(mTitle);
+
+        if(mOKButtonText!=null)
+            mOKButton.setText(mOKButtonText);
+
+        if(mOKButtonColor!=0)
+            mOKButton.setTextColor(mOKButtonColor);
+
+        if(mTitleColor!=0)
+            mTVTitle.setTextColor(mTitleColor);
+
+        if(mOKButtonClickListener!=null)
+            mOKButton.setOnClickListener(mOKButtonClickListener);
 
         for(int i=0;i<mFragmentsList.size();i++) {
             mTabLayout.addTab(mTabLayout.newTab().setText(mFragmentsList.get(i).getTitle()));
@@ -65,6 +95,26 @@ public class TabbedDialog extends DialogFragment {
 
 
         return view;
+    }
+
+    public void setTitle(String title) {
+        this.mTitle = title;
+    }
+
+    public void setTitleColor(int color) {
+        this.mTitleColor = color;
+    }
+
+    public void setOKButtonColor(int color) {
+        this.mOKButtonColor=color;
+    }
+
+    public void setOKButtonText(String text) {
+        this.mOKButtonText=text;
+    }
+
+    public void setOKButtonClickListener(View.OnClickListener listener) {
+        this.mOKButtonClickListener = listener;
     }
 
     public void addFragment(TabbedDialogTemplate fragment) {
